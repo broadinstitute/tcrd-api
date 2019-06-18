e3c0ca33a394c5ba6b6456f5dc4d1ad126de0280
Oliver Ruebenacker <oliverr@broadinstitute.org>
2019-06-18 15:35:43 -0400

    Data loading, part 6.

--- db/src/test/scala/tcrd/db/load/DataLoaderTest.scala
+++ db/src/test/scala/tcrd/db/load/DataLoaderTest.scala
@@ -2,7 +2,7 @@ package tcrd.db.load
 
 import better.files.File
 import org.scalatest.FunSuite
-import tcrd.db.Schema
+import tcrd.db.{ Record, Schema }
 import tcrd.db.Schema.{ ColBase, NumberCol, StringCol }
 
 class DataLoaderTest extends FunSuite {
@@ -22,14 +22,13 @@ class DataLoaderTest extends FunSuite {
     val dataFile = File.newTemporaryFile()
     dataFile.write(
       """
-        |id, count, greeting, stuff
-        |gene1, 1, yo, "g"
-        |gene2, 2, hello, hello
-        |gene3, 3, hi, "This ""thing"", over there"
-        |not, enough, cols
-        |a, bit, too, many, cols
+        |id,count,greeting,stuff
+        |gene1,1,yo,"g"
+        |gene2,2,hello,hello
+        |gene3,3,hi,"This ""thing"", over there"
+        |not,enough,cols
+        |a,bit,too,many,cols
       """.stripMargin.trim)
-    println(dataFile.contentAsString)
     val countCol = NumberCol("count")
     DataLoader.getRecords(dataFile, "id", Set[ColBase](countCol)) match {
       case DataLoader.LoadFailure(message) => fail(message)
@@ -37,8 +36,20 @@ class DataLoaderTest extends FunSuite {
         val geneIdCol = StringCol("id")
         assertSchema(schema, geneIdCol, countCol)
         val recordEitherList = recordIterator.toList
-        println(schema)
-        recordIterator.foreach(println)
+        assert(recordEitherList.size == 5)
+        assert(recordEitherList(0) ==
+          Right(Record("gene1", Map("id" -> "gene1", "count" -> "1", "greeting" -> "yo", "stuff" -> "g"), schema)))
+        assert(recordEitherList(1) ==
+          Right(Record(
+            "gene2",
+            Map("id" -> "gene2", "count" -> "2", "greeting" -> "hello", "stuff" -> "hello"), schema)))
+        assert(recordEitherList(2) ==
+          Right(Record(
+            "gene3",
+            Map("id" -> "gene3", "count" -> "3", "greeting" -> "hi", "stuff" -> "This \"thing\", over there"),
+            schema)))
+        assert(recordEitherList(3) == Left("Row 4 has 3 values but needs to have 4"))
+        assert(recordEitherList(4) == Left("Row 5 has 5 values but needs to have 4"))
     }
   }
 }
