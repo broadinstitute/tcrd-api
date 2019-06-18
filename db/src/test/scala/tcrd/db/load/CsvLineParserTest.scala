afc4a93dd198e33c6b49fffd3bc58aa433893447
curoli <curoli@gmail.com>
2019-06-18 14:52:05 -0400

    Loading data, part 5.

--- db/src/test/scala/tcrd/db/load/CsvLineParserTest.scala
+++ db/src/test/scala/tcrd/db/load/CsvLineParserTest.scala
@@ -7,6 +7,8 @@ class CsvLineParserTest extends FunSuite {
     assert(CsvLineParser.parseLine("apple, pear,cherry ") === Right(Seq("apple", " pear", "cherry ")))
     assert(CsvLineParser.parseLine("\"This \"\"one\"\" is quoted\",\"and this\"") ===
       Right(Seq("This \"one\" is quoted", "and this")))
+    assert(CsvLineParser.parseLine("a, \"This has a \"\" and a , in it\",b") ===
+      Right(Seq("a", "This has a \" and a , in it", "b")))
     assert(CsvLineParser.parseLine("a,,b,,,b,") === Right(Seq("a", "", "b", "", "", "b", "")))
   }
 }
--- db/src/test/scala/tcrd/db/load/DataLoaderTest.scala
+++ db/src/test/scala/tcrd/db/load/DataLoaderTest.scala
@@ -2,9 +2,22 @@ package tcrd.db.load
 
 import better.files.File
 import org.scalatest.FunSuite
-import tcrd.db.Schema.ColBase
+import tcrd.db.Schema
+import tcrd.db.Schema.{ ColBase, NumberCol, StringCol }
 
 class DataLoaderTest extends FunSuite {
+
+  def assertSchema(schema: Schema, geneIdCol: StringCol, countCol: NumberCol): Unit = {
+    assert(schema.colList.size == 4)
+    assert(schema.geneIdCol == geneIdCol)
+    assert(schema.colList(0) == geneIdCol)
+    assert(schema.colMap("id") == geneIdCol)
+    assert(schema.colList(1) == countCol)
+    assert(schema.colMap("count") == countCol)
+    assert(schema.colList(2) == schema.colMap("greeting"))
+    assert(schema.colList(3) == schema.colMap("stuff"))
+  }
+
   test("getRecords") {
     val dataFile = File.newTemporaryFile()
     dataFile.write(
@@ -17,9 +30,13 @@ class DataLoaderTest extends FunSuite {
         |a, bit, too, many, cols
       """.stripMargin.trim)
     println(dataFile.contentAsString)
-    DataLoader.getRecords(dataFile, "id", Set.empty[ColBase]) match {
-      case DataLoader.LoadFailure(message) => println(message)
+    val countCol = NumberCol("count")
+    DataLoader.getRecords(dataFile, "id", Set[ColBase](countCol)) match {
+      case DataLoader.LoadFailure(message) => fail(message)
       case DataLoader.LoadSuccess(schema, recordIterator) =>
+        val geneIdCol = StringCol("id")
+        assertSchema(schema, geneIdCol, countCol)
+        val recordEitherList = recordIterator.toList
         println(schema)
         recordIterator.foreach(println)
     }
